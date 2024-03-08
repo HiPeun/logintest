6f56418d8c89426d69437226ab193edc0be177f6
charles <rmswo417@naver.com>
2024-03-08 17:08:26 +0900

    또는 부분과 이메일 비밀번호 로그인 회원가입 아이디, 비밀번호 찾기 하고 또는 부분과 비회원 주문예약 조회 부분 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -48,91 +49,261 @@ class _MyHomePageState extends State<MyHomePage> {
           Icon(Icons.close),
         ],
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.all(30),
-                child: Text(style: TextStyle(fontSize: 45,), '로그인'),
+      body: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(30),
+              child: Text(
+                  style: TextStyle(
+                    fontSize: 45,
+                  ),
+                  '로그인'),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                width: 400,
+                height: 40,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 0.1,
+                      color: Colors.black,
+                    ),
+                    color: Colors.white),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'google.png',
+                      width: 25,
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(100, 0, 0, 0),
+                        child: Text(
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 17,
+                            ),
+                            'Google로 시작하기'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
                   padding: EdgeInsets.all(5),
                   width: 400,
                   height: 40,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 1,
+                  decoration: BoxDecoration(color: Colors.blue),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'facebook.png',
+                        width: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(70, 0, 0, 0),
+                        child: Text(
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w100),
+                            'Facebook으로 시작하기'),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  padding: EdgeInsets.all(5),
+                  width: 400,
+                  height: 40,
+                  decoration: BoxDecoration(color: Colors.yellow),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'kakao.png',
+                        width: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(100, 0, 0, 0),
+                        child: Text(
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w400),
+                            '카카오로 시작하기'),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+        Container(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 0.2,
+                  width: 160.0,
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Text(style: TextStyle(fontSize: 20), '또는'),
+                ),
+                Container(
+                  height: 0.2,
+                  width: 160,
+                  color: Colors.black,
+                )
+              ],
+            ),
+          ),
+        ),
+        Column(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(9, 4, 0, 0),
+                child: Text(
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                    '이메일'),
+              ),
+              width: 400,
+              height: 40,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 0.2,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(9, 4, 0, 0),
+            child: Text(
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+                '비밀번호'),
+          ),
+          width: 400,
+          height: 40,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 0.2,
+            ),
+          ),
+        ),
+        Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Container(
+                  width: 400,
+                  height: 40,
+                  color: Colors.lightBlueAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(175, 6, 0, 0),
+                    child: Text(style: TextStyle(fontSize: 15), '로그인'),
+                  ),
+                ),
+              ),
+              Container(
+                width: 400,
+                height: 30,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          '회원가입'),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(203, 0, 0, 0),
+                            child: Text(
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                '아이디 ・ 비밀번호 찾기'),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 0.2,
+                        width: 160.0,
                         color: Colors.black,
                       ),
-                      color: Colors.white),
-                  child: Text(
-                    textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 17,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(style: TextStyle(fontSize: 20), '또는'),
                       ),
-                      'Google로 시작하기'),
+                      Container(
+                        height: 0.2,
+                        width: 160,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
           ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  width: 400,
-                  height: 40,
-                  decoration: BoxDecoration(
-
-                      color: Colors.blue),
-                  child: Text(
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w100
-                      ),
-                      'Facebook으로 시작하기'),
+        ),
+        Container(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Container(
+                width: 400,
+                height: 40,
+                color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(135, 6, 0, 0), child: Text(style: TextStyle(fontSize: 15,color: Colors.white), '비회원 주문예약 조회'),
                 ),
               ),
-            ],
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  width: 400,
-                  height: 40,
-                  decoration: BoxDecoration(
-
-                      color: Colors.yellow),
-                  child: Text(
-
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400
-                      ),
-                      '카카오로 시작하기'),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+            ),
+          ]),
+        ),
+      ]),
     );
   }
 }
-
